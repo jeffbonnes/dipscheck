6eff24c827aeff1cf9e3bd5e5cfebe7f6b735384
Jeff Bonnes <jeffb@gameshape.com>
2013-08-27 11:00:44 +1000

    Change défaut unit to dips

--- Resources/app.js
+++ Resources/app.js
@@ -20,37 +20,37 @@ var win = Titanium.UI.createWindow({
 win.orientationModes = [Ti.UI.PORTRAIT];
 
 var image = Ti.UI.createImageView({
-	top : '10dp',
-	width : '100dp',
-	height : '100dp',
+	top : 10,
+	width : 100,
+	height : 100,
 	backgroundColor : 'blue',
 	image : '/images/common/myimage.png'
 });
 
 var label = Ti.UI.createLabel({
-	height : '20dp',
-	top : '120dp',
+	height : 20,
+	top : 120,
 	text : '100dp = ' + dpsToPixels(100) + 'px',
 	textAlign : 'center'
 });
 
 var labelWidth = Ti.UI.createLabel({
-	height : '20dp',
-	top : '150dp',
+	height : 20,
+	top : 150,
 	text : 'screen width = ' + pixelsToDps(width) + 'dp, ' + width + 'px',
 	textAlign : 'center'
 });
 
 var labelHeight = Ti.UI.createLabel({
-	height : '20dp',
-	top : '180dp',
+	height : 20,
+	top : 180,
 	text : 'screen height = ' + pixelsToDps(height) + 'dp, ' + height + 'px',
 	textAlign : 'center'
 });
 
 var labelDpi = Ti.UI.createLabel({
-	height : '20dp',
-	top : '210dp',
+	height : 20,
+	top : 210,
 	text : 'screen dpi = ' + dpi,
 	textAlign : 'center'
 });
